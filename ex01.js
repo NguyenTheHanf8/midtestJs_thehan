f4fe815a3583e2c724e9c0b86cb9cc626d7a8596
NguyenTheHanf8 <nguyenthehan03@gmail.com>
2024-10-02 15:45:28 +0700

    Nộp bài midtest

--- ex01.js
+++ ex01.js
@@ -3,10 +3,10 @@ function taxiFare(km) {
     return "Invalid ";
   } else if (km > 0 && km <= 1) {
     postage = 5000;
-  } else if (km > 1 && km <= 5) {
+  } else if (km <= 30) {
     postage = 5000 + (km - 1) * 4000;
   } else {
-    postage = 5000 + 4 * 4000 + (km - 5) * 3000;
+    postage = 5000 + 4 * 4000 + (km - 30) * 3000;
   }
   return postage;
 }
